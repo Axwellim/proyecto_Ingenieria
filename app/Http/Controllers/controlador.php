eb23f0643f656f9fc5288d8546d8de45c2294324
Jefferson <jefersonadeee@gmail.com>
2025-04-26 16:33:04 -0500

    carpetas MVC

---
+++ app/Http/Controllers/controlador.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Usuario;
+use Tymon\JWTAuth\Facades\JWTAuth;
+
+class controlador extends Controller
+    {
+        
+    }
